5f16744fec94a4374251716f88fe09b5588847c1
Naga <naga@users.noreply.github.com>
2026-09-18 04:08:52 +0000

    Bump cache to v5 to serve updated Price/Medicine/Micro-Investment tools

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "savings-lab-v4";
+const CACHE_NAME = "savings-lab-v5";
 const APP_SHELL = [
   "./", "./index.html", "./index-flip-cards.html", "./manifest.webmanifest",
   "./icon.svg", "./icon-192.svg", "./icon-512.svg", "./pwa.js", "./autodebit-tracker.html", "./bank-fees-tracker.html",
